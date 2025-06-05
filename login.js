e0fcbec27bc870d0632a9f1c4dc5970e8dbf8589
chibuzor94 <chibuzorgoodluck9411@gmail.com>
2025-06-05 22:37:26 +0100

    add login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+let username = "student";
+let password = "abc123";
+
+if (username == "student" && password == "abc123") {
+  console.log("Login Successful");
+} else {
+  console.log("Invalid Credentials");
+}
+
+let a = 1;
+let b = "1";
+console.log(a === b);
