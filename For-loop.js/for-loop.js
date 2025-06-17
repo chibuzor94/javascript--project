be2bb6d073e6bf2f867b00178394d317ba251c72
chibuzor94 <chibuzorgoodluck9411@gmail.com>
2025-06-17 18:22:23 +0100

    for-loop.js

---
+++ For-loop.js/for-loop.js
@@ -0,0 +1,6 @@
+// for loop repeats some codes for a limited or known number of times.
+// for loops contains, initial expression, condition, increment/decrement.
+
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
