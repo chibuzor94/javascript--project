6c7e23dfced8c8310d2d2559a2a6b2030b264faf
chibuzor94 <chibuzorgoodluck9411@gmail.com>
2025-06-26 02:37:07 +0100

    functions-assignment.js

---
+++ FUNCTIONS/functions-assignment.js
@@ -0,0 +1,35 @@
+function multiplyTwoNums(a, b){
+    return a * b
+}
+console.log(multiplyTwoNums(3, 4))
+
+
+function addThreeNums(a, b, c){
+    return a + b + c
+}
+console.log(addThreeNums(2, 4, 5))
+
+
+function convertMinutesToSeconds(x, y){
+    return minutes * 60;
+}
+const minutes = 2
+const seconds = 
+console.log(convertMinutesToSeconds(2))
+
+
+
+function doubleNumber(a){
+    return number * 2;
+
+}
+const number = 50
+console.log(doubleNumber(50))
+
+function areaOfRectangle(lenght, width){
+    return lenght * width
+}
+console.log(areaOfRectangle(3, 5))
+
+//let area = areaOfRectangle(3, 5);
+//console.log(area)
